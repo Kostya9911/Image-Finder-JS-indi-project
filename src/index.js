39814c19587d96d30f2bbb18935cafcb0438ac72
Kostya9911 <koshka5557@i.ua>
2023-01-24 15:57:06 +0200

    start

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// ============Axios==============
+const axios = require('axios');
+
+// ============Notiflix===========
+import Notiflix from 'notiflix';
+// ============Simplelightbox=====
+import SimpleLightbox from 'simplelightbox';
+// console.log(axios);
+// console.log(Notiflix);
+// console.log(SimpleLightbox);
